95f8c654b4e4290ba17bada7f29c93aa41a278b8
joel.pettersson90@gmail.com <joel.pettersson90@gmail.com>
2022-02-04 11:01:46 +0100

    created project about testing in angular

---
+++ testing/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { TestingComponent } from './testing/testing.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TestingComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
